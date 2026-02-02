dce7692b572e480fa463884d3ff531983bf90462
Денис <altynsarynastudio@gmail.com>
2026-02-02 21:02:14 +0600

    Small refactoring sv_notification

--- lua/homigrad/sv_notification.lua
+++ lua/homigrad/sv_notification.lua
@@ -2,15 +2,50 @@ util.AddNetworkString("HGNotificate")
 util.AddNetworkString("HGNotificateBerserk")
 
 --local hg_old_notificate = ConVarExists("hg_old_notificate") and GetConVar("hg_old_notificate") or CreateConVar("hg_old_notificate",0,FCVAR_SERVER_CAN_EXECUTE,"enable old notifications (chatprints)",0,1)
-local hev_color = Color(255,125,0)
-local function CreateNotification(ply, msg, delay, msgKey, showTime, func, clr)
-    if ply.organism and ply.organism.otrub then return end
-    if ply.PlayerClassName and ply.PlayerClassName == "Gordon" and clr != hev_color then return end
-    if msg == "" then return end
+local hev_color = Color(255, 125, 0)
+
+local function ValidateNotification(ply, clr, msg)
     if not IsValid(ply) or not ply:IsPlayer() then error("player is not valid!") return false end
-    if not msg or not isstring(msg) then error("no message or message is invalid!") return false end
+    
+    if !ply.organism then return false end
+    if ply.organism.otrub then return false end
+    if ply.PlayerClassName and ply.PlayerClassName == "Gordon" and clr != hev_color then return false end
+    if not msg or not isstring(msg) or msg == "" then error("no message or message is invalid!") return false end
+
+    return true
+end
+
+local function TotalNotification(ply, msgKey, clr, clr2, msg, berserk, func)
+    if !IsValid(ply) then return end
+    if !ply.msgs[msgKey] then return end
+
+    if (ply.organism and ply.organism.otrub) or !ply:Alive() then
+        return
+    end
+
+    if ply.organism and ply.organism.pain > 60 and (!clr or clr.g > 250) then
+        return
+    end
+
+    if berserk and func and isfunction(func) then
+        if func(ply) then
+            return
+        end
+    end
+
+    local netmessage = (berserk and "HGNotificateBerserk") or "HGNotificate"
+    net.Start(netmessage)
+        net.WriteString(msg)
+        net.WriteColor(clr2)
+    net.Send(ply)
+end
+
+local function CreateNotification(ply, msg, delay, msgKey, showTime, func, clr)
+    if not ValidateNotification(ply, clr, msg) then return false end
+    
     msgKey = msgKey or msg
     ply.msgs = ply.msgs or {}
+    
     if msgKey and ply.msgs[msgKey] then
         if isnumber(ply.msgs[msgKey]) then
             if ply.msgs[msgKey] > CurTime() then
@@ -24,10 +59,11 @@ local function CreateNotification(ply, msg, delay, msgKey, showTime, func, clr)
     delay = delay or 0
 
     if msgKey then ply.msgs[msgKey] = delay and (not isnumber(delay) or CurTime() + delay) or nil end
+    
     --показывать один раз за промежуток времени
     --(если delay не номерок то оно пинганет в следующей жизни)
 
-    if ply.organism and ply.organism.brain > 0.1 then
+    if ply.organism.brain > 0.1 then
         for i = 1, utf8.len(msg) do
             if math.random(3) == 1 and msg[i] != "?" and msg[i] != "." then
                 msg = hg.replace_by_index(msg, i, (math.random(1,2) > 1 and "m" or "b") )
@@ -41,40 +77,18 @@ local function CreateNotification(ply, msg, delay, msgKey, showTime, func, clr)
     local clr2 = Color(clr.r, clr.g, clr.b, 255)
 
     timer.Simple(showTime, function()
-        if !IsValid(ply) then return end
-        if !ply.msgs[msgKey] then return end
-
-        if (ply.organism and ply.organism.otrub) or !ply:Alive() then
-            return
-        end
-
-        if ply.organism and ply.organism.pain > 60 and (!clr or clr.g > 250) then
-            return
-        end
-
-        if func and isfunction(func) then
-            if func(ply) then return end
-        end
-
-        net.Start("HGNotificate")
-        net.WriteString(msg)
-        //net.WriteFloat(showTime or 3)
-        net.WriteColor(clr2)
-        net.Send(ply)
+        TotalNotification(ply, msgKey, clr, clr2, msg, false, nil) -- Функция nil, так как берсерк выключен, и она исполняется только когда он true.
     end)
 
     return true
 end
 
-//erm it's ass but i don't care enough
 local function CreateNotificationBerserk(ply, msg, delay, msgKey, showTime, func, clr)
-    if ply.organism and ply.organism.otrub then return end
-    if ply.PlayerClassName and ply.PlayerClassName == "Gordon" and clr != hev_color then return end
-    if msg == "" then return end
-    if not IsValid(ply) or not ply:IsPlayer() then error("player is not valid!") return false end
-    if not msg or not isstring(msg) then error("no message or message is invalid!") return false end
+    if not ValidateNotification(ply, clr, msg) then return false end
+    
     msgKey = msgKey or msg
     ply.msgs = ply.msgs or {}
+    
     if msgKey and ply.msgs[msgKey] then
         if isnumber(ply.msgs[msgKey]) then
             if ply.msgs[msgKey] > CurTime() then
@@ -86,7 +100,6 @@ local function CreateNotificationBerserk(ply, msg, delay, msgKey, showTime, func
     end
 
     delay = delay or 0
-
     if msgKey then ply.msgs[msgKey] = delay and (not isnumber(delay) or CurTime() + delay) or nil end
     --показывать один раз за промежуток времени
     --(если delay не номерок то оно пинганет в следующей жизни)
@@ -94,7 +107,7 @@ local function CreateNotificationBerserk(ply, msg, delay, msgKey, showTime, func
         func(ply)
     end
 
-    if ply.organism and ply.organism.brain > 0.1 then
+    if ply.organism.brain > 0.1 then
         for i = 1, utf8.len(msg) do
             if math.random(3) == 1 and msg[i] != "?" and msg[i] != "." then
                 msg = hg.replace_by_index(msg, i, (math.random(1,2) > 1 and "m" or "b") )
@@ -106,24 +119,8 @@ local function CreateNotificationBerserk(ply, msg, delay, msgKey, showTime, func
 
     local clr = clr or color_white
     local clr2 = Color(clr.r, clr.g, clr.b, 255)
-
     timer.Simple(showTime, function()
-        if !IsValid(ply) then return end
-        if !ply.msgs[msgKey] then return end
-
-        if (ply.organism and ply.organism.otrub) or !ply:Alive() then
-            return
-        end
-
-        if ply.organism and ply.organism.pain > 60 and (!clr or clr.g > 250) then
-            return
-        end
-
-        net.Start("HGNotificateBerserk")
-        net.WriteString(msg)
-        //net.WriteFloat(showTime or 3)
-        net.WriteColor(clr2)
-        net.Send(ply)
+        TotalNotification(ply, msgKey, clr, clr2, msg, true, func)
     end)
 
     return true

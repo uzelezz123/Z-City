f89f1f650db04ee91862b885e2d5c6e959d5450e
sadsalat <74601305+sadsalat@users.noreply.github.com>
2026-04-08 17:34:02 +0300

    кем - rtv alias

--- gamemodes/zcity/gamemode/libraries/rtv/sv_rtv.lua
+++ gamemodes/zcity/gamemode/libraries/rtv/sv_rtv.lua
@@ -487,8 +487,7 @@ function zb.CheckRTVVotes(needPrint)
     
     return false
 end
-
-COMMANDS.rtv = {function(ply, args)
+local function rtv(ply, args)
     --print(zb.votestarted)
 	if zb.votestarted then
 		zb.RTVMenu(ply)
@@ -543,8 +542,11 @@ COMMANDS.rtv = {function(ply, args)
 
     if zb.CheckRTVVotes(true) then
         return
-    end
-end, 0}
+    end 
+end
+
+COMMANDS.rtv = {rtv, 0}
+COMMANDS.кем = {rtv, 0}
 
 hook.Add("ShutDown", "ResetRTVVotesOnMapChange", zb.ClearRTVVotes)
 hook.Add("PostGamemodeLoaded", "InitializeRTVSystem", function()
